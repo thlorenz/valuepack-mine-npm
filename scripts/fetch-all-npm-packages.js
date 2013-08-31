9371d9c5d89f542e91b06c6d05580503c62a75e0
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-31 17:43:07 -0400

    using http to get to npmjs instead of https in order to avoid certificate (UNABLE_TO_VERIFY_LEAF_SIGNATURE) problem
    - also improved logging while at it

--- scripts/fetch-all-npm-packages.js
+++ scripts/fetch-all-npm-packages.js
@@ -5,9 +5,10 @@
 
 var path           =  require('path')
   , fs             =  require('fs')
+  , log            =  require('valuepack-core/util/log')
   , packagesStream =  require('../lib/fetch-all-npm-packages')
   , dataDir        =  process.env.VALUEPACK_DATA || path.join(__dirname, '..', 'data')
   , jsonPath       =  path.join(dataDir, 'npm-packages.json')
 
-console.log('Storing fetched packages data at: ', jsonPath);
+log.info('script/fetch-users', 'Storing fetched packages data at: ', jsonPath);
 packagesStream().pipe(fs.createWriteStream(jsonPath, { encoding: 'utf8' }))
--- scripts/fetch-npm-users.js
+++ scripts/fetch-npm-users.js
@@ -3,11 +3,13 @@
 'use strict';
 /*jshint asi: true */
 
+
 var path        =  require('path')
   , fs          =  require('fs')
+  , log         =  require('valuepack-core/util/log')
   , usersStream =  require('../lib/fetch-all-npm-users')
   , dataDir     =  process.env.VALUEPACK_DATA || path.join(__dirname, '..', 'data')
   , jsonPath    =  path.join(dataDir, 'npm-users.json')
 
-console.log('Storing fetched users data at: ', jsonPath);
+log.info('script/fetch-users', 'Storing fetched users data at: ', jsonPath);
 usersStream().pipe(fs.createWriteStream(jsonPath, { encoding: 'utf8' }))
